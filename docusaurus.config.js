c69abdea6c8b76fd97c92d742accba77d9bf3e5f
GuiBibeau <g.bibeaulaviolette@gmail.com>
2023-01-23 12:48:15 -0500

    review comments, add gitignore for vercel test files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,7 +45,7 @@ const config = {
           ],
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css") ,
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
