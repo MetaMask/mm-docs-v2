fe79a3d03061dee9aba27ccd2eb365d9eaa18f43
shanejonas <shanejonas@users.noreply.github.com>
2023-03-30 16:54:14 +0000

    deploy: c1e2099c25b604966e60ca902af717b5e2ff6fb7

--- staging/assets/js/1df93b7f.61141688.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmm_docs_v_2=self.webpackChunkmm_docs_v_2||[]).push([[3237],{9754:(e,t,c)=>{c.r(t),c.d(t,{default:()=>u});var l=c(7294),s=c(6550);function u(){return l.createElement(s.l_,{to:"/wallet"})}}}]);
